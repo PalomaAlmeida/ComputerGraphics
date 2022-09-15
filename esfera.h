b21b3579219e454f2bacdb0a0b3744d5a3ddbce1
akaMiike <mike.nss@alu.ufc.br>
2022-09-15 11:35:08 -0300

    Criação da classe de luz e refatoração na main

--- esfera.h
+++ esfera.h
@@ -3,6 +3,7 @@
 
 #include "vetor.h"
 #include "raio.h"
+#include "luz_pontual.h"
 #include <cmath>
 
 using namespace std;
@@ -10,14 +11,17 @@ using namespace std;
 class esfera{
     public:
         esfera(){}
-        esfera(const ponto& centro_esfera, const double raio): centro(centro_esfera), raio(raio){}
+        esfera(const ponto& centro, const double raio): centro(centro), raio(raio){}
+
+        ponto centro_esfera() {return centro;}
+        double raio_esfera() { return raio; }
 
         pair<double,double> calcular_raizes_intersecao(const raio& r){
-             vetor p = r.origem() - centro;
+             vetor p = r.origem() - centro_esfera();
 
             auto a = produto_vetor(r.direcao(),r.direcao());
             auto b = 2.0 * produto_vetor(p, r.direcao());
-            auto c = produto_vetor(p, p) - raio*raio;
+            auto c = produto_vetor(p, p) - raio_esfera()*raio_esfera();
 
             auto descriminante = b*b - 4*a*c;
             if(descriminante < 0){
@@ -29,9 +33,35 @@ class esfera{
             return {t1,t2};
         }
 
+        double calcular_intensidade_luz_esfera(const raio& direcao_luz, double raiz_mais_proxima, const luz_pontual& ponto_luz, int exp_especular){
+            double i = 0;
+
+            auto p_int = direcao_luz.origem() + raiz_mais_proxima*direcao_luz.direcao();
+
+            auto vetor_normal_esfera = p_int - centro_esfera();
+            auto L = ponto_luz.posicao_ponto()- p_int;
+            auto normal_esfera = vetor_normal_esfera/vetor_normal_esfera.comprimento();
+            auto n_dot_l = produto_vetor(normal_esfera/normal_esfera.comprimento(),L);
+
+            if(n_dot_l > 0){
+                i += ponto_luz.intensidade_luz() * n_dot_l  / (normal_esfera.comprimento() * L.comprimento()) ;
+            }
+
+            if(exp_especular != -1){
+                auto R = 2*normal_esfera*n_dot_l - L;
+                auto r_dot_l = produto_vetor(R,-direcao_luz.direcao());
+                if(r_dot_l > 0){
+                i += ponto_luz.intensidade_luz() * pow(r_dot_l/(R.comprimento()*L.comprimento()),exp_especular);
+                }
+            }
+
+            return i;
+        }
+
     public:
         ponto centro;
         double raio;
+    
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -3,40 +3,16 @@
 #include "vetor.h"
 #include "raio.h"
 #include "esfera.h"
+#include "luz_pontual.h"
 #include <cmath>
 
 using namespace std;
 
 auto posicao_luz1 = ponto(0,5,0);
 auto intensidade_luz = 0.7;
+luz_pontual luz1(posicao_luz1, intensidade_luz);
+
 auto expoente_especular = 1000;
-
-double calcular_intensidade_luz_esfera(const raio& direcao_luz, const ponto& centro_esfera, double raiz_mais_proxima, const ponto& posicao_luz, const int exp_especular){
-  double i = 0;
-
-  auto p_int = direcao_luz.origem() + raiz_mais_proxima*direcao_luz.direcao();
-
-  auto vetor_normal_esfera = p_int - centro_esfera;
-  auto L = posicao_luz - p_int;
-  auto normal_esfera = vetor_normal_esfera/vetor_normal_esfera.comprimento();
-  auto n_dot_l = produto_vetor(normal_esfera/normal_esfera.comprimento(),L);
-
-  if(n_dot_l > 0){
-    i += intensidade_luz * n_dot_l  / (normal_esfera.comprimento() * L.comprimento()) ;
-  }
-
-  if(exp_especular != -1){
-    auto R = 2*normal_esfera*n_dot_l - L;
-    auto r_dot_l = produto_vetor(R,-direcao_luz.direcao());
-    if(r_dot_l > 0){
-      i += intensidade_luz * pow(r_dot_l/(R.comprimento()*L.comprimento()),exp_especular);
-    }
-  }
-
-  return i;
-
-}
-
 
 int main() {
   
@@ -77,7 +53,7 @@ int main() {
         auto raizes = e1.calcular_raizes_intersecao(r);
         
         if(!isinf(raizes.first) && !isinf(raizes.second)){
-          auto intensidade_luz_difusa = calcular_intensidade_luz_esfera(r,centro_esfera,min(raizes.first,raizes.second), posicao_luz1, expoente_especular);
+          auto intensidade_luz_difusa = e1.calcular_intensidade_luz_esfera(r,min(raizes.first,raizes.second), luz1, expoente_especular);
           cor_pixel = cor(255,0,0) * intensidade_luz_difusa;
         }
         else{

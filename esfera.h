06913319c083381442f0cf34422e44fbd563145a
akaMiike <mike.nss@alu.ufc.br>
2022-10-06 00:18:37 -0300

    Adicionado sombras entre objetos

--- esfera.h
+++ esfera.h
@@ -1,23 +1,24 @@
 #ifndef SPHERE_H
 #define SPHERE_H
 
-#include "vetor.h"
 #include "raio.h"
+#include "vetor.h"
 #include "luz_pontual.h"
 #include <cmath>
+#include <list>
 
 using namespace std;
 
-class esfera{
+class Esfera{
     public:
-        esfera(){}
-        esfera(const ponto& centro, const double raio): centro(centro), raio(raio){}
+        Esfera(){}
+        Esfera(const ponto& centro, const double raio): centro(centro), raio(raio){}
 
         ponto centro_esfera() {return centro;}
         double raio_esfera() { return raio; }
 
-        pair<double,double> calcular_raizes_intersecao(const raio& r){
-             vetor p = r.origem() - centro_esfera();
+        pair<double,double> calcular_raizes_intersecao(const Raio& r){
+            vetor p = r.origem() - centro_esfera();
 
             auto a = produto_vetor(r.direcao(),r.direcao());
             auto b = 2.0 * produto_vetor(p, r.direcao());
@@ -33,15 +34,20 @@ class esfera{
             return {t1,t2};
         }
 
-        double calcular_intensidade_luz_esfera(const raio& direcao_luz, double raiz_mais_proxima, const luz_pontual& ponto_luz, int exp_especular){
+        double calcular_intensidade_luz_esfera(list<Esfera> esferas, const Raio& direcao_luz, double raiz_mais_proxima, const luz_pontual& ponto_luz, int exp_especular, double luz_ambiente){
+            
             double i = 0;
+            ponto p = direcao_luz.origem() + raiz_mais_proxima*direcao_luz.direcao();
 
-            auto p_int = direcao_luz.origem() + raiz_mais_proxima*direcao_luz.direcao();
-
-            auto vetor_normal_esfera = p_int - centro_esfera();
-            auto L = ponto_luz.posicao_ponto()- p_int;
+            auto vetor_normal_esfera = p - centro_esfera();
+            auto L = ponto_luz.posicao_ponto()- p;
             auto normal_esfera = vetor_normal_esfera/vetor_normal_esfera.comprimento();
             auto n_dot_l = produto_vetor(normal_esfera/normal_esfera.comprimento(),L);
+
+            Raio p_int(p,L);
+            pair<Esfera, double> esfera_e_raizes_sombra = calcular_raiz_mais_proxima_interseccao(esferas,p_int,0.001,1);
+            //cout << "Entrou aqui: " << esfera_e_raizes_sombra.second << "\n";
+            if(esfera_e_raizes_sombra.second != INFINITY){return luz_ambiente;}
 
             if(n_dot_l > 0){
                 i += ponto_luz.intensidade_luz() * n_dot_l  / (normal_esfera.comprimento() * L.comprimento()) ;
@@ -55,8 +61,30 @@ class esfera{
                 }
             }
 
-            return i;
+            return i + luz_ambiente;
         }
+
+
+        static pair<Esfera,double> calcular_raiz_mais_proxima_interseccao(list<Esfera> esferas, Raio& r, double t_min, double t_max){
+            double raiz_mais_proxima = INFINITY;
+            Esfera esfera_mais_proxima;
+
+            for(auto i= esferas.begin(); i!=esferas.end(); i++){
+                auto raizes = i->calcular_raizes_intersecao(r);
+                    
+                if(raizes.first < raiz_mais_proxima && (raizes.first >= t_min && raizes.first < t_max)){
+                    raiz_mais_proxima = raizes.first;
+                    esfera_mais_proxima = *i;
+                }
+                if(raizes.second < raiz_mais_proxima && (raizes.second >= t_min && raizes.second < t_max)){
+                    raiz_mais_proxima = raizes.second;
+                    esfera_mais_proxima = *i;
+                }
+                    
+            }
+            return {esfera_mais_proxima, raiz_mais_proxima};
+
+            }
 
     public:
         ponto centro;
--- main.cpp
+++ main.cpp
@@ -5,14 +5,34 @@
 #include "esfera.h"
 #include "luz_pontual.h"
 #include <cmath>
+#include <list>
 
 using namespace std;
 
 auto posicao_luz1 = ponto(0,5,0);
 auto intensidade_luz = 0.7;
+auto intensidade_luz_ambiente = 0.1;
+
 luz_pontual luz1(posicao_luz1, intensidade_luz);
 
+list<Esfera> esferas;
+list<luz_pontual> luzes;
+
 auto expoente_especular = 1000;
+
+cor calcular_cor_pixel(list<Esfera> esferas, Esfera esfera_mais_proxima, double raiz_mais_proxima, Raio& r, double luz_ambiente){
+  cor cor_pixel;
+    
+  if(!isinf(raiz_mais_proxima)){
+    auto intensidade_luz_difusa = esfera_mais_proxima.calcular_intensidade_luz_esfera(esferas, r,raiz_mais_proxima, luz1, expoente_especular, luz_ambiente);
+    cor_pixel = cor(255,0,0) * intensidade_luz_difusa;
+  }
+  else{
+    cor_pixel = cor(100,100,100);
+  }
+
+  return cor_pixel;
+}
 
 int main() {
   
@@ -34,32 +54,29 @@ int main() {
     // Origem (olho do observador)
     auto origem = ponto(0, 0, 0);
 
-    double raio_esfera = 0.3;
-    auto centro_esfera = -(ponto(0,0,dJanela)+ponto(0,0,raio_esfera));  
+    double raio_esfera1 = 0.3;
+    auto centro_esfera1 = -(ponto(0,0,dJanela)+ponto(0,0,raio_esfera1));  
+
+    double raio_esfera2 = 1.0;
+    auto centro_esfera2 = -(ponto(0.5,1.5,dJanela)+ponto(0,0,raio_esfera2));
   
+    esferas.push_back(Esfera(centro_esfera1, raio_esfera1));
+    esferas.push_back(Esfera(centro_esfera2,raio_esfera2));
+
     for (int j = 0; j < altura_imagem; ++j) {
       for (int i = 0; i < largura_imagem; ++i) { 
-        cor cor_pixel;
 
         //Coordenadas do centro do quadrado da "tela de mosquito" (pixel)
         auto x = -(wJanela / 2) + Dx/2 + i*Dx;
         auto y = (hJanela/2) - Dy/2 - j*Dy;
 
         //Raio que sai da origem para o pixel
-        raio r(origem, ponto(x,y,-dJanela));
-        esfera e1(centro_esfera, raio_esfera);
+        Raio r(origem, ponto(x,y,-dJanela));
 
-        //Calcula as raizes, verifica se há interseção entre o raio e a esfera e retorna o par de raízes se houver
-        auto raizes = e1.calcular_raizes_intersecao(r);
-        
-        if(!isinf(raizes.first) && !isinf(raizes.second)){
-          auto intensidade_luz_difusa = e1.calcular_intensidade_luz_esfera(r,min(raizes.first,raizes.second), luz1, expoente_especular);
-          cor_pixel = cor(255,0,0) * intensidade_luz_difusa;
-        }
-        else{
-          cor_pixel = cor(100,100,100);
-        }
-        
+        //Calcula as raizes, verifica se há interseção entre o raio e as esferas e retorna a cor do pixel
+        pair<Esfera,double> esfera_e_raiz_mais_proximas = Esfera().calcular_raiz_mais_proxima_interseccao(esferas,r,1.0,(double) INFINITY);
+        cor cor_pixel = calcular_cor_pixel(esferas,esfera_e_raiz_mais_proximas.first, esfera_e_raiz_mais_proximas.second, r, intensidade_luz_ambiente);
+
         pintar(std::cout, cor_pixel);
       }
     }
